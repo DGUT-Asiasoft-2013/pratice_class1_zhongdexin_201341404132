87de3ff51c860ee04384ac3f6291895ca39f6ecd
dasenzhong <727745374@qq.com>
2016-12-10 09:33:54 +0800

    第八次

--- app/src/main/java/com/example/helloworld/BootActivity.java
+++ app/src/main/java/com/example/helloworld/BootActivity.java
@@ -3,10 +3,16 @@ package com.example.helloworld;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class BootActivity extends Activity {
 
@@ -22,15 +28,52 @@ public class BootActivity extends Activity {
 	protected void onResume() {
 		super.onResume();
 		
-		Handler handler = new Handler();
-		handler.postDelayed(new Runnable() {
-			private int abcd = 0;
-			
-			public void run() {
-				startLoginActivity();
+//		Handler handler = new Handler();
+//		handler.postDelayed(new Runnable() {
+//			private int abcd = 0;
+//
+//			public void run() {
+//				startLoginActivity();
+//			}
+//		}, 1000);
+
+		OkHttpClient client = new OkHttpClient();
+
+		Request request = new Request.Builder()
+				.url("http://172.27.0.56:8080/membercenter/api/hello")
+				.method("GET",null)
+				.build();
+
+		//enqueue是入队的意思，enqueue（）是将request请求加入队列中等待执行
+		client.newCall(request).enqueue(new Callback() {
+			@Override
+			public void onFailure(Call call, final IOException e) {
+				BootActivity.this.runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						Toast.makeText(BootActivity.this,e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+					}
+				});
 			}
-		}, 1000);
-		
+
+			@Override
+			public void onResponse(Call call, final Response response) throws IOException {
+				Log.d("response", response.toString());
+
+//				BootActivity.this.runOnUiThread(new Runnable() {
+//
+//					@Override
+//					public void run() {
+//						try {
+//							Toast.makeText(BootActivity.this, response.body().string(), Toast.LENGTH_SHORT).show();
+//						} catch (IOException e) {
+//							e.printStackTrace();
+//						}
+//						startLoginActivity();
+//					}
+//				});
+			}
+		});
 	}
 	
 	void startLoginActivity(){
